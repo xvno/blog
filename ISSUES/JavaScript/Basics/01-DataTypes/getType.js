674a46b48608dbd4321aeeaacf3be17971ccbd98
xvno <x@vno.io>
2021-02-07 14:50:14 +0800

    Opt: getType.js: 优化代码

--- ISSUES/JavaScript/Basics/01-DataTypes/getType.js
+++ ISSUES/JavaScript/Basics/01-DataTypes/getType.js
@@ -1,13 +1,10 @@
 module.exports = (function () {
     const fn = Object.prototype.toString;
     const startIdx = 8;
+    const endIdx = -1;
 
-    const getType = function (obj, idx = startIdx, end) {
+    return function getType(obj, idx = startIdx, end = endIdx) {
         let raw = fn.call(obj);
-        let len = raw.length;
-        end = end || len - 1;
-        return raw.substring(idx, end);
+        return raw.slice(idx, end);
     };
-
-    return getType;
 })();
